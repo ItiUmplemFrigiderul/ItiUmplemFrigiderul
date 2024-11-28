ce372a9a6a5031f1ce80098c617a228a1f110037
radudeaconu <radu_ad2000@yahoo.com>
2024-11-28 12:34:12 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ItiUmplemFrigiderul.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
