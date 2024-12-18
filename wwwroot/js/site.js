c427751e3f18ad0672d416d216c4af8c530baaf5
alelena14 <cioritaelena14@gmail.com>
2024-12-18 17:55:42 +0200

    review merge sa iau rating ul din stelute

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const stars = document.querySelectorAll(".stars i");
+const ratingInput = document.getElementById("Rating"); // Câmpul input ascuns
 
-// Write your JavaScript code.
+stars.forEach((star, index1) => {
+    // Efect la hover
+    star.addEventListener("mouseover", () => {
+        stars.forEach((star, index2) => {
+            index1 >= index2
+                ? (star.style.transform = "scale(1.2)")
+                : (star.style.transform = "scale(1)");
+        });
+    });
+
+    // Anulare hover
+    star.addEventListener("mouseout", () => {
+        stars.forEach(star => star.style.transform = "scale(1)");
+    });
+
+    // Click pentru selectare
+    star.addEventListener("click", () => {
+        stars.forEach((star, index2) => {
+            index1 >= index2
+                ? star.classList.add("activestar")
+                : star.classList.remove("activestar");
+        });
+        ratingInput.value = index1 + 1; // Setează valoarea ratingului
+        console.log("Rating selectat:", ratingInput.value); // Testare
+    });
+});
