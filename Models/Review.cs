c7fe5102a71d06bcec660715ded27b8a09acffca
alelena14 <cioritaelena14@gmail.com>
2024-12-18 14:26:58 +0200

    test

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,23 +1,130 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using ItiUmplemFrigiderul.Data;
+using ItiUmplemFrigiderul.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
-namespace ItiUmplemFrigiderul.Models
+namespace ArticlesApp.Controllers
 {
-    public class Review
+    public class CommentsController : Controller
     {
-        [Key]
-        public int Id { get; set; }
-        public int FarmProductId { get; set; }
-        public string UserId { get; set; }
-        [Required(ErrorMessage = "Produsul este obligatoriu")]
-        public virtual FarmProduct FarmProduct { get; set; }
-        public virtual ApplicationUser User { get; set; }
+        // PASUL 10: useri si roluri 
 
-        [Required(ErrorMessage = "Nota este obligatorie")]
-        public int Rating { get; set; }
-        public string? Content { get; set; }
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public CommentsController(
+        ApplicationDbContext context,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager
+        )
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        /*
+        
+        // Adaugarea unui comentariu asociat unui articol in baza de date
+        [HttpPost]
+        public IActionResult New(Comment comm)
+        {
+            comm.Date = DateTime.Now;
 
+            if(ModelState.IsValid)
+            {
+                db.Comments.Add(comm);
+                db.SaveChanges();
+                return Redirect("/Articles/Show/" + comm.ArticleId);
+            }
+
+            else
+            {
+                return Redirect("/Articles/Show/" + comm.ArticleId);
+            }
+
+        }
+
+        
+        */
+
+
+        // Stergerea unui comentariu asociat unui articol din baza de date
+        // Se poate sterge comentariul doar de catre userii cu rolul de Admin 
+        // sau de catre utilizatorii cu rolul de User sau Editor, doar daca 
+        // acel comentariu a fost postat de catre acestia
+
+        [HttpPost]
+        [Authorize(Roles = "User,Editor,Admin")]
+        public IActionResult Delete(int id)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                db.Comments.Remove(comm);
+                db.SaveChanges();
+                return Redirect("/Articles/Show/" + comm.ArticleId);
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa stergeti comentariul";
+                TempData["messageType"] = "alert-danger";
+                return RedirectToAction("Index", "Articles");
+            }
+        }
+
+        // In acest moment vom implementa editarea intr-o pagina View separata
+        // Se editeaza un comentariu existent
+        // Editarea unui comentariu asociat unui articol
+        // [HttpGet] - se executa implicit
+        // Se poate edita un comentariu doar de catre utilizatorul care a postat comentariul respectiv 
+        // Adminii pot edita orice comentariu, chiar daca nu a fost postat de ei
+
+        [Authorize(Roles = "User,Editor,Admin")]
+        public IActionResult Edit(int id)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                return View(comm);
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa editati comentariul";
+                TempData["messageType"] = "alert-danger";
+                return RedirectToAction("Index", "Articles");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User,Editor,Admin")]
+        public IActionResult Edit(int id, Comment requestComment)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                if (ModelState.IsValid)
+                {
+                    comm.Content = requestComment.Content;
+
+                    db.SaveChanges();
+
+                    return Redirect("/Articles/Show/" + comm.ArticleId);
+                }
+                else
+                {
+                    return View(requestComment);
+                }
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa editati comentariul";
+                TempData["messageType"] = "alert-danger";
+                return RedirectToAction("Index", "Articles");
+            }
+        }
     }
 }
