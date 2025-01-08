94a785d008659e6bcc16886fa1d4f99d462c8eb2
radudeaconu <radu_ad2000@yahoo.com>
2025-01-08 17:39:05 +0200

    cart done order p acolo

--- Data/Migrations/20250108125843_M13.Designer.cs
+++ Data/Migrations/20250108125843_M13.Designer.cs
@@ -4,6 +4,7 @@ using ItiUmplemFrigiderul.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ItiUmplemFrigiderul.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250108125843_M13")]
+    partial class M13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -104,8 +107,8 @@ namespace ItiUmplemFrigiderul.Data.Migrations
                     b.Property<int>("FarmProductId")
                         .HasColumnType("int");
 
-                    b.Property<int>("Quantity")
-                        .HasColumnType("int");
+                    b.Property<float>("Quantity")
+                        .HasColumnType("real");
 
                     b.Property<string>("UserId")
                         .IsRequired()
